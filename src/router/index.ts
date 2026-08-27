046a9dbf54eda107ac07da1ab0dbc516d8fafe2a
anna condal <acondalv@gmail.com>
2026-08-27 15:26:43 +0200

    feat: add routing and views

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import PlanetDetailView from '@/views/PlanetDetailView.vue'
+import PlanetsView from '@/views/PlanetsView.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      redirect: '/planets',
+    },
+    {
+      path: '/planets',
+      name: 'planets',
+      component: PlanetsView,
+    },
+    {
+      path: '/planets/:id',
+      name: 'planet-detail',
+      component: PlanetDetailView,
+    },
+  ],
 })
 
 export default router
